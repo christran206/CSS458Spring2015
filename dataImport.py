9d3b457e9dac2d9a7fbe0633fa0b165069bb0db0
kparker831 <kparker831@gmail.com>
2015-05-24 20:22:11 -0700

    updated importFaculty method to create list of professors.
    updated importFaculty method to create list of professors.

--- dataImport.py
+++ dataImport.py
@@ -4,6 +4,8 @@
 ## Methods to import classes and faculty
 ##------------------------------------------------------------------------------
 import numpy as N
+from professor import Professor
+#from course import Course
 
 def importCourses(path ='classes.csv', delimit = ',', type = 'str', header = 1):
     
@@ -24,12 +26,13 @@ def importCourses(path ='classes.csv', delimit = ',', type = 'str', header = 1):
     courses = N.genfromtxt(path, delimiter = delimit, \
     dtype = type, skip_header=header)
     
+       
     return courses
 
 def importFaculty(path ='faculty.csv', delimit = ',', type = 'str', header = 1):
     
     """
-    Import Faculty generates a numpy array of faculty from a data file
+    Import Faculty generates a list of professor objects from input file.
     
     Inputs:
         *path - The path to the course data file
@@ -38,13 +41,26 @@ def importFaculty(path ='faculty.csv', delimit = ',', type = 'str', header = 1):
         *header - number of rows at top to ignore for header data
         
     Outputs:
-        A details x number of faculty numpy array of faculty information
+        A list of professor objects defined by the data in the input file.
     
     """
     
+    # Generate an array of data from the file at input path
     faculty = N.genfromtxt(path, delimiter = delimit, \
     dtype = type, skip_header=header)
     
-    return faculty
+    # Create a list of professors to return
+    professors = []
+    
+    # Iterate through the faculty array, creating a professor object for each
+    for i in xrange(N.shape(faculty)[0]):
+        nextFac = Professor(faculty[i][0],faculty[i][1],float(faculty[i][2]), \
+        int(faculty[i][3]), faculty[i][4], faculty[i][6], faculty[i][6])
+        
+        # Append each new professor to the list of professors
+        professors.append(nextFac)
+        
+    # Return professors
+    return professors
 
 
