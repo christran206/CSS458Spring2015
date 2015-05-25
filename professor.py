b032b6f86e242603ceb10878190e7930ee9810b3
kparker831 <kparker831@gmail.com>
2015-05-24 19:52:46 -0700

    professor class
    stores professor objects, will be created initially from array ready in
    from faculty.csv

---
+++ professor.py
@@ -0,0 +1,10 @@
+class Professor:
+
+    def __init__(self, name, fullTime, classes, students, exp1, exp2, exp3):
+        self.name = name
+        self.fullTime = fullTime
+        self.classes = classes
+        self.students = students
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.exp3 = exp3
