93e696b4e19e67df97f9c5cf863af5126e60d7cb
jwdailey <jason2393@msn.com>
2015-05-27 15:29:27 -0700

    Schedules Part-Time Profs
    Based off flow charts and code for full-time professors, the part-time
    professors have been scheduled. Still needs tested, so there is a break
    statement at the start of the loop to prevent it from running (line 72).

--- Scheduler.py
+++ Scheduler.py
@@ -43,10 +43,10 @@ class Scheduler:
                     courses = self.Schedule.courses[quarter]
                     # get only courses where professor has an expertise
                     compatiblecourses = [course for course in courses if course.expertise in professor.expertise and course.instructor == None]
-                    # shuffle courses
                     if len(compatiblecourses) == 0:
                         professorNotCompatCount += 1
                         break
+                    # shuffle courses 
                     random.shuffle(compatiblecourses)
                     # find a course that the professor can teach and if the professor is able to teach
                     # check if professor has reached the limit
@@ -69,4 +69,52 @@ class Scheduler:
         # At this point, remaining courses need to be populated with available part time instructors
         for professor in parttime:
             # TO DO
-            print "TODO"
+            break
+            #shuffle professors
+            random.shuffle(parttime)
+            # iterate through professors
+            remainingProfs = len(parttime)
+            professorNotCompatCount = [0 for i in xrange(len(parttime))]
+            i = -1
+            while remainingProfs > 0:
+                for professor in parttime:
+                    i = (i + 1) % len(parttime)
+                    # get the current course allotment of the professor
+                    allotment = 0
+                    for coursequarter in professor.teaching:
+                        for course in professor.teaching[coursequarter]:
+                            courseLevel = course.number/100
+                            if courseLevel == 1:
+                                allotment += 0.5
+                            else:
+                                allotment += 1.0
+                    # if the professor can't teach anymore, break loop to the next professor
+                    # Also check if the professor has gone through 4 loops without compatible courses
+                    if allotment >= professor.classamount or professorNotCompatCount[i] >= 4:
+                        remainingProfs -= 1
+                        continue
+                    # get only courses where professor has an expertise
+                    compatiblecourses = [course for course in courses if course.expertise in professor.expertise and course.instructor == None]
+                    if len(compatiblecourses) == 0:
+                        professorNotCompatCount += 1
+                        continue
+                    # shuffle courses 
+                    random.shuffle(compatiblecourses)
+                    # find a course that the professor can teach and if the professor is able to teach
+                    # check if professor has reached the limit
+                    for course in compatiblecourses:
+                        # check if the course overlaps with any existing professor courses in the quarter
+                        for existingcourse in professor.teaching[quarter]:
+                            if course.courseTimeOverlap(existingcourse):
+                                continue
+                        courseLevel = course.number/100
+                        if courseLevel == 1:
+                            if professor.classamount - allotment >= 0.5:
+                                course.instructor = professor
+                                professor.teaching[quarter].append(course)
+                        else:
+                            if professor.classamount - allotment >= 1.0:
+                                course.instructor = professor
+                                professor.teaching[quarter].append(course)
+                        break
+                        
