694c1da255e120085bc7574eb2d424a1f7aa26f4
jwdailey <jason2393@msn.com>
2015-05-27 12:20:23 -0700

    professorClassAssignment changed to objects
    Corrected all array-based syntax to follow current object-based format.

--- professorClassAssignment.py
+++ professorClassAssignment.py
@@ -1,6 +1,4 @@
 ##------------------------------------------------------------------------------
-## NOT WORKING YET
-##
 ## CSS 458 Final Project
 ## Christopher Tran, Jason Dailey, Kevin Parker
 ## Methods to import classes and faculty
@@ -13,26 +11,23 @@ def assignProfessors(professors, courses):
     Loop through professors and assign professors classes up to specified limit
     
     Inputs:
-        *professors - The list of professors, and all relevant data
-        *courses - The list of courses, and all relevant data
-        
-    Outputs:
-        A multi-level dictionary containing course info and assigned professor
+        professors - The list of professors, and all relevant data
+        courses - The list of courses, and all relevant data
     
     """
     for professor in professors:
-        numClasses = 0
+        numCredits = 0
         #need professor object
         availableCourses = specialty(professor, courses)
-        while numClasses < professor[2]:
-            addClass(professor, availableCourses)
-        #   availableCourses = specialty(professor, courses)
-        #a = 0
+        quarter = 0
+        #quarter: 0=fall, 1=winter, 2=spring, 3=summer
+        while numCredits < (professor.classamount) * 5: #5-credit classes
+            credits = addClass(professor, availableCourses, quarter)
+            quarter = (quarter + 1) % 4 #Rotates thru the 4 quarters
+            numCredits += credits
+    
 
-    schedule = 0
-    return schedule
-
-def specialty(professor, courses):
+def specialty(professor, courses): 
     """
     Loop through the courses, finding ones the professor can teach
     
@@ -40,15 +35,16 @@ def specialty(professor, courses):
         A shortened course list specific to the professor's specialties
     
     """
-    summer = [4]
-    autumn = [1]
-    winter = [2]
-    spring = [3]
+    autumn = []
+    winter = []
+    spring = []
+    summer = []
+    
     for course in courses:
-        if course[5] == professor[4] or \
-            course[5] == professor[5] or \
-            course[5] == professor[6]:
-                n = course[4]
+        if course.expertise == professor.expertise[0] or \
+            course.expertise == professor.expertise[1] or \
+            course.expertise == professor.expertise[2]:
+                n = course.quarter
                 if n == 'Autumn':
                     autumn.append(course)
                 elif n == 'Winter':
@@ -61,9 +57,20 @@ def specialty(professor, courses):
                 
     return (autumn, winter, spring, summer)
     
-def addClass(professor, availableCourses):
+    
+def addClass(professor, availableCourses, quarter):
     """
     Marks a professor as an instructor for a class in the list
     
+    Inputs:
+        professor - The current professor to assign to a class
+        availableCourses - The list of courses the prof specializes in
+        quarter - The quarter to assign the professor a class (0=Fall, 1=Winter,
+        3=Spring, 4=Summer)
+        
+    Outputs:
+        The number of credits of the class assigned
     """
-    return 0
+    availableCourses[quarter][0].instructor = professor
+        
+    return availableCourses[quarter][0].credits
